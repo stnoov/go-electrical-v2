eb6ae4200f425ffcf4f29fb2bb7536d5e62a44b7
Artem Sitnov <stnoov@gmail.com>
2020-12-27 22:02:34 +0200

    Add meta tag to restrict zoom in inputs

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
